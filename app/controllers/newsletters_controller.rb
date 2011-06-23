9b4eb03efe8fbdcd3efcd5d566a70db73f50612d
David Bennett <davidbennett@bravevision.com>
2011-06-23 17:21:17 +0930

    .deliver!

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -3,7 +3,7 @@ class NewslettersController < ApplicationController
     
     @newsletter = Newsletter.find(params[:id])
     user = User.find(params[:id])
-    NewsletterMailer.send_newsletter(user, @newsletter)
+    NewsletterMailer.send_newsletter(user, @newsletter).deliver
     render 'newsletter_mailer/send_newsletter', :layout => 'newsletter'
   end
 end
