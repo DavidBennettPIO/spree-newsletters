99fbdebd60739480c014e12224914f688c8f41db
David Bennett <davidbennett@bravevision.com>
2012-06-18 17:42:56 +0930

    fix admin_tab_newsletters

--- lib/overrides/spree_newsletters_hooks.rb
+++ lib/overrides/spree_newsletters_hooks.rb
@@ -3,9 +3,3 @@ Deface::Override.new(:virtual_path => "layouts/admin",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
                      :text => "<%= tab(:newsletters) %>",
                      :disabled => false)
-                     
-Deface::Override.new(:virtual_path => "layouts/admin",
-                     :name => "admin_tab_newsletters",
-                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab(:newsletters) %>",
-                     :disabled => false)
