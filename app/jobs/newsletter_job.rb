c5ff3be1a9f67140aaff97f63ecdd9c43d4276ee
David Bennett <davidbennett@bravevision.com>
2014-08-26 15:37:12 +0930

    make sure all admins get an email.

--- app/jobs/newsletter_job.rb
+++ app/jobs/newsletter_job.rb
@@ -9,7 +9,18 @@ class NewsletterJob < Struct.new(:sids, :newsletter_id)
     if !sids.nil?
       recipients = Spree::NewsletterRecipient.where(:state_id => sids).all
     else
-      recipients = Spree::NewsletterRecipient.where(:email => ['davidbennett@bravevision.com','admin@triggahappi.com.au']).all
+
+      emails = []
+      Spree::Role.where(name: 'admin').first.users.each do |user|
+        admin_recip = Spree::NewsletterRecipient.where( email: user.email )
+        if admin_recip.nil?
+          c ||= Spree::Country.first
+          s ||= Spree::State.first
+          Spree::NewsletterRecipient.create( name: 'Admin Test', email: user.email, country: c, state: s )
+        end
+        emails << user.email
+      end
+      recipients = Spree::NewsletterRecipient.where(email: emails).all
     end
     
     recipients.each do |recipient|
